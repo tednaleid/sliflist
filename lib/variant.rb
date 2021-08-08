fbb7a4a3f802e3823e8f8826c8effac9c6b163ef
Robert Slifka <robert.slifka@gmail.com>
2021-08-07 19:24:18 -0700

    Initial crack

---
+++ lib/variant.rb
@@ -0,0 +1,11 @@
+class Variant
+
+  attr_reader :name
+  attr_reader :perks
+
+  def initialize(name, perks)
+    @name = name
+    @perks = perks
+  end
+
+end
---
+++ spec/lib/variant_spec.rb
@@ -0,0 +1,29 @@
+describe Variant do
+
+  subject {
+    Variant.new('TEST_NAME', {
+      'barrels' => [Perk.from_name('Arrowhead Brake')],
+      'magazines' => [Perk.from_name('Accurized Rounds')],
+      'perks1' => [Perk.from_name('Backup Plan')],
+      'perks2' => [Perk.from_name('Rampage')],
+      'masterworks' => [Perk.from_name('Range MW')]
+    })
+  }
+
+  describe '#name' do
+    it 'returns the name' do
+      expect(subject.name).to eq('TEST_NAME')
+    end
+  end
+
+  describe '#perks' do
+    it 'returns the perks' do
+      expect(subject.perks.length).to eq(5)
+      expect(subject.perks).to include('barrels' => [Perk.from_name('Arrowhead Brake')])
+      expect(subject.perks).to include('magazines' => [Perk.from_name('Accurized Rounds')])
+      expect(subject.perks).to include('perks1' => [Perk.from_name('Backup Plan')])
+      expect(subject.perks).to include('perks2' => [Perk.from_name('Rampage')])
+      expect(subject.perks).to include('masterworks' => [Perk.from_name('Range MW')])
+    end
+  end
+end
